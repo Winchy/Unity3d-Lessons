a2d6317f9cca3e6161ab66f3777a549831b597f5
Winchy <winchyyi@gmail.com>
2016-06-21 16:18:01 +0800

    add renderimage lesson

---
+++ Unity3d Lessons/Assets/RenderImage/FullScreenWave.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class FullScreenWave : MonoBehaviour {
+
+    [SerializeField]
+    private Shader transShader;
+
+    [SerializeField, Range(0.001f, 0.5f)]
+    private float waveLength = 0.02f;
+
+    [SerializeField, Range(1f, 100f)]
+    private float frequence = 10.0f;
+
+    private Material transMaterial;
+
+    Material material
+    {
+        get
+        {
+            if (transMaterial == null)
+            {
+                transMaterial = new Material(transShader);
+                transMaterial.hideFlags = HideFlags.HideAndDontSave;
+            }
+            return transMaterial;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!SystemInfo.supportsImageEffects)
+        {
+            enabled = false;
+            return;
+        }
+
+        if (!transShader && !transShader.isSupported)
+        {
+            enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        material.SetFloat("_WaveLength", waveLength);
+        material.SetFloat("_Frequence", frequence);
+    }
+
+    void OnRenderImage(RenderTexture sourceTexture, RenderTexture destTexture)
+    {
+        if (transShader != null)
+        {
+            material.SetColor("_Color", new Color(0.5f, 0.5f, 0, 1.0f));
+            Graphics.Blit(sourceTexture, destTexture, material);
+        }
+        else
+        {
+            Graphics.Blit(sourceTexture, destTexture);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (transShader)
+        {
+            DestroyImmediate(transMaterial);
+        }
+    }
+}
---
+++ Unity3d Lessons/Assets/RenderImage/WaveShader.shader
@@ -0,0 +1,53 @@
+﻿Shader "Hidden/Wave"
+{
+	Properties
+	{
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+	_WaveLength("Wave Length", Range(0.001, 0.2)) = 0.02
+		_Frequence("Frequence", Range(1, 100)) = 10
+	}
+
+		SubShader
+	{
+		Pass
+	{
+		CGPROGRAM
+#pragma vertex vert_img
+#pragma fragment frag
+#pragma fragmentoption ARB_precision_hint_fastest
+#include "UnityCG.cginc"
+
+		uniform sampler2D _MainTex;
+	half _WaveLength;
+	half _Frequence;
+
+	half2 horizontalConvert(half2 uv) {
+		half2 newUV = uv;
+		newUV.y += sin(uv.x * 100 + _Time * 100) / 200;
+		return newUV;
+	}
+
+	half2 radialConvert(half2 uv) {
+		half2 newUV = uv;
+		half3 dir = half3(newUV - half2(0.5, 0.5), 0);
+		//half3 n = half3(0, 0, 1);
+		//half3 p = cross(dir, n);
+		//p = normalize(p);
+		fixed l = length(dir.xy);
+		newUV += sin(l / _WaveLength + _Time.y * _Frequence)  * _WaveLength * normalize(dir.xy);
+		return newUV;
+	}
+
+	fixed4 frag(v2f_img i) : COLOR
+	{
+		//half2 newUV = horizontalConvert(i.uv);
+		half2 newUV = radialConvert(i.uv);
+		fixed4 renderTex = tex2D(_MainTex, newUV);
+		return renderTex;
+	}
+
+		ENDCG
+	}
+	}
+		FallBack off
+}
